97762c917aac5a20ba4bde1cf6b23dc1a01d8064
Khisamov Alexey <hisamov@bifit.com>
2024-11-21 12:51:01 +0500

    Strings tests

--- elements/handlers/string_handlers.py
+++ elements/handlers/string_handlers.py
@@ -3,16 +3,16 @@ import allure
 
 class StringRequester:
     def __init__(self, requester):
-        self.endpoint = 'object/'
-        self.requester = requester
+        self.__endpoint = 'object/'
+        self.__requester = requester
 
     def get_strings(self):
         with allure.step("Получения списка строк"):
-            return self.requester.get_request(f"{self.endpoint}string")
+            return self.__requester.get_request(f"{self.__endpoint}string")
 
     def get_string(self, id):
         with allure.step(f"Получения строки {id=}"):
-            return self.requester.get_request(f"{self.endpoint}string/{id}")
+            return self.__requester.get_request(f"{self.__endpoint}string/{id}")
 
     def post_strings(self, data):
-        return self.requester.post_request(f"{self.endpoint}string", data=data)
+        return self.__requester.post_request(f"{self.__endpoint}string", data=data)
